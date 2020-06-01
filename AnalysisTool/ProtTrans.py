b826761033fb747f29321ca77dc402b2fe9f4b16
Mikey Spurr <master@Mikeys-MacBook-Pro-2.local>
2020-05-31 22:03:42 -0700

    Transcription and translation: This test will take whatever 2 Dna files entered, and transcribe it to RNA then translate it into Proteins. It will then compare the two files and inform us where the differences between the files are located.

--- AnalysisTool/ProtTrans.py
+++ AnalysisTool/ProtTrans.py
@@ -1,9 +1,7 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
-# from biopython import Bio
-# from biopython.Bio import Seq
 
-myFile_str = input("  Enter File :") #Enter file name to read from
+myFile_str = raw_input("  Enter File :") #Enter file name to read from
 #data_str = open(myFile_str)	#opens file
 #data_str.close()	#close file
 
@@ -14,27 +12,27 @@ listt=[]
 for record in SeqIO.parse(myFile_str,'fasta'):
     id = record.id
     seq_a = record.seq
-    print ('File Name: ', myFile_str) #prints the entered files name
-    print ('Name: ', id)	#prints the name of the DNA sequence
-    print ('Size: ', len(seq_a))	#prints the length of the sequence
-    #print ('Sequence: ', seq_a)		#prints out the whole DNA sequence
+    print 'File Name: ', myFile_str #prints the entered files name
+    print 'Name: ', id	#prints the name of the DNA sequence
+    print 'Size: ', len(seq_a)	#prints the length of the sequence
+    print 'Sequence: ', seq_a		#prints out the whole DNA sequence
 
     RNAfromDNA_str  = seq_a.transcribe() # gives RNA sequence
     #DNAfromRNA_str  = seq.back_transcribe(RNAfromDNA_str) # gives DNA sequence from the RNA conversion
-    PROTfromRNA_strA = RNAfromDNA_str.translate()
+    PROTfromRNA_str = RNAfromDNA_str.translate()
 
 
-    # print (" Original DNA  :",seq_a) #prints DNA sequence of mut.fasta
-    # print (" RNA from DNA  :",RNAfromDNA_str) #prints RNA sequence of mut.fasta
-    # #print " DNA from RNA  :",DNAfromRNA_str
-    # print (" PROT from RNA :",PROTfromRNA_str) #prints the protiens of mut.fasta
+    print " Original DNA  :",seq_a #prints DNA sequence of mut.fasta
+    print " RNA from DNA  :",RNAfromDNA_str #prints RNA sequence of mut.fasta
+    #print " DNA from RNA  :",DNAfromRNA_str
+    print " PROT from RNA :",PROTfromRNA_str #prints the protiens of mut.fasta
 
-    #print Counter(seq)			#counts each of the bases in the DNA sequence
-    #	print Counter(myFile_str(seq))
+    # print Counter(seq)			#counts each of the bases in the DNA sequence
+    # 	print Counter(myFile_str(seq))
 
 
 
-    myFile_str = input("  Enter File :") #Enter file name to read from
+    myFile_str = raw_input("  Enter File :") #Enter file name to read from
     dna = myFile_str
         #data_str = open(myFile_str)	#opens file
         #data_str.close()
@@ -43,32 +41,32 @@ for record in SeqIO.parse(myFile_str,'fasta'):
 
         id = record.id
         seq_b = record.seq
-        print ('File Name: ', myFile_str) #prints the entered files name
-        print ('Name: ', id)	#prints the name of the DNA sequence
-        print ('Size: ', len(seq_b))	#prints the length of the sequence
-        #print ('Sequence: ', seq_b)	#prints out the whole DNA sequence
+        print 'File Name: ', myFile_str #prints the entered files name
+        print 'Name: ', id	#prints the name of the DNA sequence
+        print 'Size: ', len(seq_b)	#prints the length of the sequence
+        print 'Sequence: ', seq_b	#prints out the whole DNA sequence
 
 
     RNAfromDNA_str  = seq_b.transcribe() # gives RNA sequence
     #DNAfromRNA_str  = seq.back_transcribe(RNAfromDNA_str) # gives DNA sequence from the RNA conversion
-    PROTfromRNA_strB = RNAfromDNA_str.translate() #translating RNA into DNA
+    PROTfromRNA_str = RNAfromDNA_str.translate() #translating RNA into DNA
 
 
-    # print (" Original DNA  :",seq_b) #prints wild.fasta sequence
-    # print (" RNA from DNA  :",RNAfromDNA_str) #prints the RNA of wild.fasta sequence
+    print " Original DNA  :",seq_b #prints wild.fasta sequence
+    print " RNA from DNA  :",RNAfromDNA_str #prints the RNA of wild.fasta sequence
     #print " DNA from RNA  :",DNAfromRNA_str
-    # print (" PROT from RNA :",PROTfromRNA_str) #prints the protiens of wild.fasta sequence
+    print " PROT from RNA :",PROTfromRNA_str #prints the protiens of wild.fasta sequence
 
 
 
     # note: a method to compare two entered sequences
-    # print (" This is the compareSequences() method")
-    # print (" seq_a :",seq_a)
-    # print (" seq_b :",seq_b)
+    print " This is the compareSequences() method"
+    print " seq_a :",seq_a
+    print " seq_b :",seq_b
 
 for i in range(len(seq_a)):
     #print " checking: seqA_str, current char: ",i, seqA_str[i]
-    if seq_a[i] == seq_b[i]:
-        print ("\n Sequences are the same at position :", i)
-        print ("   seqA_str[i] = ",PROTfromRNA_strA[i])
-        print ("   seqB_str[i] = ",PROTfromRNA_strB[i])
+    if seq_a[i] != seq_b[i]:
+        print "\n Sequences are different at position :", i
+        print "   seqA_str[i] != ",seq_a[i]
+        print "   seqB_str[i] != ",seq_b[i]
